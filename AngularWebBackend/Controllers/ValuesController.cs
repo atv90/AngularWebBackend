7979aa2f1518b68b2eafe25f4ec2f9af28135ef6
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2018-09-18 09:05:58 +0300

    lisätty Values-kontrolleriin OrderCount-toiminto, joka paluttaa arvon -1

--- AngularWebBackend/Controllers/ValuesController.cs
+++ AngularWebBackend/Controllers/ValuesController.cs
@@ -9,31 +9,39 @@ namespace AngularWebBackend.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        [HttpGet] //mahdollistaa GET-pyynnöt
+        public int OrderCount()
         {
-            return new string[] { "value1", "value2" };
+            return -1;
         }
 
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
 
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// GET api/values
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
 
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
+        //// GET api/values/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
